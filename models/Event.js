c1a376683ca8e0bd7b54e50106e3698044f9bded
felipemendes <f.ribeiromendes@gmail.com>
2018-07-27 22:54:26 -0300

    Create pagination, quantity and search for Events

--- models/Event.js
+++ models/Event.js
@@ -2,14 +2,23 @@ const db = require('../db-connect');
 
 const Event = {
 
-    getAllEvents: function (callback) {
+    getEvents: function (id, search, page = 1, quantity = 10, callback) {
 
-        return db.query("SELECT * FROM events", callback);
-    },
+        var query = "SELECT * FROM events ";
 
-    getEventById: function (id, callback) {
+        const validPage = page != 0 ? page : 1;
+        const validQuantity = quantity != 0 ? quantity : 10;
+        const currentPage = (validPage - 1) * validQuantity;
+        const eventById = id == undefined ? "" : " id=" + id;
+        const eventBySearch = search == undefined ? "" : " place LIKE '%" + search + "%' ";
 
-        return db.query("SELECT * FROM events WHERE id=?", [id], callback);
+        var whereClause = "";
+        if (eventById != "" && eventBySearch != "") whereClause = "WHERE" + eventById + " AND" + eventBySearch;
+        if (eventById != "" && eventBySearch == "") whereClause = "WHERE" + eventById;
+        if (eventById == "" && eventBySearch != "") whereClause = "WHERE" + eventBySearch;
+
+        query += whereClause + " LIMIT " + validQuantity + " OFFSET " + currentPage + "";
+        return db.query(query, callback);
     },
 
     addEvent: function (Event, File, Uuid, callback) {
@@ -23,7 +32,7 @@ const Event = {
     },
 
     updateEvent: function (id, Event, File, callback) {
-        
+
         return db.query("UPDATE events SET user_login=?, update_time=?, url_image=?, place=?, date=?, address=?, sale_place=?, sale_place_phone=? where Id=?", [Event.user_login, new Date().toLocaleString(), File.path, Event.place, Event.date, Event.address, Event.sale_place, Event.sale_place_phone, id], callback);
     },
 };
